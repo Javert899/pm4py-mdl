adb3e28c008cb1f987323ba837a69718e3363656
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-09-10 07:49:06 +0200

    Added gzip compression for Parquet exporting

--- pm4pymdl/objects/mdl/exporter/factory.py
+++ pm4pymdl/objects/mdl/exporter/factory.py
@@ -1,5 +1,6 @@
 from pm4py.objects.log.exporter.parquet import factory as parquet_exporter
 from pm4pymdl.algo.mvp.utils import exploded_mdl_to_succint_mdl
+from copy import deepcopy
 
 def apply(df, file_path, parameters=None):
     if parameters is None:
@@ -14,4 +15,6 @@ def apply(df, file_path, parameters=None):
     if file_path.endswith(".csv") or file_path.endswith(".mdl"):
         df.to_csv(file_path, index=False, sep=',', quotechar='\"')
     else:
-        parquet_exporter.export_df(df, file_path, parameters=parameters)
+        new_parameters = deepcopy(parameters)
+        new_parameters["compression"] = "gzip"
+        parquet_exporter.export_df(df, file_path, parameters=new_parameters)
