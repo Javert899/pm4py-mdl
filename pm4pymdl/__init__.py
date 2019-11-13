58d51acd4aafe1e27aab0e8e83806ef28e7337f8
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-11-13 08:54:21 +0100

    Increased version number

--- pm4pymdl/__init__.py
+++ pm4pymdl/__init__.py
@@ -1,6 +1,6 @@
 from pm4pymdl import algo, objects, visualization, order_log_generation
 
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 __doc__ = "Process Mining for Python - Multi-Dimensional Event Logs"
 __author__ = 'PADS'
 __author_email__ = 'a.berti@pads.rwth-aachen.de'
