649849e0dcd621375548ddc2a834832a3b75f146
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-11-18 13:08:10 +0100

    Small change to MDL importer

--- pm4pymdl/objects/mdl/importer/factory.py
+++ pm4pymdl/objects/mdl/importer/factory.py
@@ -5,8 +5,11 @@ def apply(file_path, parameters=None):
     if parameters is None:
         parameters = {}
 
+    sep = parameters["sep"] if "sep" in parameters else ","
+    quotechar = parameters["quotechar"] if "quotechar" in parameters else "\""
+
     if file_path.endswith(".csv") or file_path.endswith(".mdl"):
-        df = pd.read_csv(file_path, sep=',', quotechar='\"')
+        df = pd.read_csv(file_path, sep=sep, quotechar=quotechar)
         df.type = "succint"
         return df
     elif file_path.endswith(".parquet"):
