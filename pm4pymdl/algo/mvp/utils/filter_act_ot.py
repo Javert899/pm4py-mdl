b4ab1296f3d549a1a6f9c06119fc8ebbb0cf54f6
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-02-18 14:56:21 +0100

    Fixed filter

--- pm4pymdl/algo/mvp/utils/filter_act_ot.py
+++ pm4pymdl/algo/mvp/utils/filter_act_ot.py
@@ -6,19 +6,21 @@ def filter_ot(df, act, ot, v1, v2, parameters=None):
     if parameters is None:
         parameters = {}
 
+    print(len(df))
+
     try:
         if df.type == "succint":
             df = succint_mdl_to_exploded_mdl.apply(df)
     except:
         pass
 
-    red_df = df[df["event_activity"] == act]
-    red_df = red_df.dropna(subset=[ot])
+    red_df0 = df[df["event_activity"] == act]
+    red_df = red_df0.dropna(subset=[ot])
 
     dct = red_df.groupby("event_id").size().to_dict()
     lst = [x for x,y in dct.items() if v1 <= y <= v2]
 
-    red_df = red_df[red_df["event_id"].isin(lst)]
+    red_df = red_df0[red_df0["event_id"].isin(lst)]
 
     return filter_metaclass.do_filtering(df, red_df, parameters=parameters)
 
