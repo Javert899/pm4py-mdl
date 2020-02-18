1e1f7286d7d2818348d8ec2fd2474f08e68df409
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-02-18 15:01:24 +0100

    distr_act_otype distribution

---
+++ pm4pymdl/algo/mvp/utils/distr_act_otype.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+
+def get(dataframe, activity, ot, parameters=None):
+    if parameters is None:
+        parameters = {}
+
+    ret = {}
+    red_df = dataframe[dataframe["event_activity"] == activity]
+    red_df = red_df.dropna(axis=1, how="all")
+    red_df = red_df.dropna(subset=[ot])
+
+    lista = [y for x,y in dict(red_df.groupby("event_id").size()).items()]
+
+    return dict(Counter(lista))
+
