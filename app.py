ecd8a74ab5f8ea3f37eff0fb9b2572cae880408f
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-02-26 11:12:17 +0100

    visibility onchange event

--- app.py
+++ app.py
@@ -10,6 +10,18 @@ class Shared:
 app = Flask(__name__)
 
 
+@app.route("/act_ot_selection/<process>")
+def act_ot_selection(process=None):
+    session = request.cookies.get('session') if 'session' in request.cookies else uuid.uuid4()
+    response = make_response(render_template(
+        'act_ot_selection.html',
+        Process=Shared.logs[process].get_controller(session)
+        ))
+    if not request.cookies.get('session'):
+        response.set_cookie('session', str(uuid.uuid4()))
+    return response
+
+
 @app.route("/cases_view/<process>")
 def cases_view(process=None):
     session = request.cookies.get('session') if 'session' in request.cookies else uuid.uuid4()
