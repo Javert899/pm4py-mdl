c15fa1cf63410cd977685e374ac00bc646a91714
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-02-27 07:03:15 +0100

    Added requirements

--- setup.py
+++ setup.py
@@ -36,7 +36,10 @@ setup(
     license='GPL 3.0',
     install_requires=[
         "pm4py==1.2.11",
-        "pyarrow==0.15.1"
+        "pyarrow==0.15.1",
+        "Flask",
+        "flask-cors",
+        "setuptools"
     ],
     project_urls={
         'Documentation': 'http://pm4py.pads.rwth-aachen.de/documentation/',
