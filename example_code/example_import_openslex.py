1a1fb6693c6b075ba6b49d34c58560c154373e0a
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-04-07 08:32:09 +0200

    Added examples for XOC and OpenSLEX

---
+++ example_code/example_import_openslex.py
@@ -0,0 +1,8 @@
+from pm4pymdl.objects.openslex.importer import factory as mdl_importer
+from pm4pymdl.algo.mvp.gen_framework import factory as discovery
+from pm4pymdl.visualization.mvp.gen_framework import factory as vis_factory
+
+df = mdl_importer.apply("../example_logs/openslex/metamodel.slexmm")
+model = discovery.apply(df, model_type_variant="model1", node_freq_variant="type1", edge_freq_variant="type11")
+gviz = vis_factory.apply(model, parameters={"format": "svg"})
+vis_factory.view(gviz)
---
+++ example_code/example_import_xoc.py
@@ -0,0 +1,8 @@
+from pm4pymdl.objects.xoc.importer import factory as xoc_importer
+from pm4pymdl.algo.mvp.gen_framework import factory as discovery
+from pm4pymdl.visualization.mvp.gen_framework import factory as vis_factory
+
+df = xoc_importer.apply("../example_logs/xoc/log.xoc")
+model = discovery.apply(df, model_type_variant="model1", node_freq_variant="type1", edge_freq_variant="type11")
+gviz = vis_factory.apply(model, parameters={"format": "svg"})
+vis_factory.view(gviz)
