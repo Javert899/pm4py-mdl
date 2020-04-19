adf88b202c11fbcc213040b1daf66ebf08e76f9b
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-04-19 11:23:17 +0200

    intermediate commit

--- extractor.py
+++ extractor.py
@@ -3,6 +3,7 @@ import pandas as pd
 from frozendict import frozendict
 from pm4pymdl.algo.mvp.gen_framework import factory as discovery
 from pm4pymdl.visualization.mvp.gen_framework import factory as vis_factory
+from pm4pymdl.algo.mvp.utils import clean_frequency, clean_arc_frequency
 
 dir = r"C:\Users\aless\Documents\sap_extraction"
 
@@ -21,6 +22,15 @@ def read_activities():
         Shared.activities[row["TCODE"]] = row["TTEXT"]
 
 
+def remove_zeros(stru):
+    i = 0
+    while i < len(stru):
+        if not stru[i] == "0":
+            return stru[i:]
+        i = i + 1
+    return stru
+
+
 def extract_cdhdr():
     cdhdr = pd.read_csv(os.path.join(dir, "cdhdr.tsv"), sep="\t")
     cdhdr = cdhdr[["OBJECTCLAS", "OBJECTID", "USERNAME", "UDATE", "UTIME", "TCODE", "CHANGENR"]]
@@ -32,15 +42,78 @@ def extract_cdhdr():
     merged = pd.merge(merged, cdhdr, left_on="VALUE_NEW", right_on="OBJECTID", suffixes=["", "_3"])
     merged = merged.rename(columns={"USERNAME": "event_resource", "TCODE": "event_activity"})
     merged["event_timestamp"] = merged["UDATE"] + " " + merged["UTIME"]
-    merged["event_timestamp"] = pd.to_datetime(merged["event_timestamp"])
+    merged["event_timestamp"] = pd.to_datetime(merged["event_timestamp"], format="%d.%m.%Y %H:%M:%S")
     merged = merged.dropna(subset=["event_activity"])
+    merged = merged.dropna(subset=["event_resource"])
     stream = merged.to_dict("r")
     for ev in stream:
+        ev["OBJECTID"] = remove_zeros(ev["OBJECTID"])
+        ev["OBJECTID_3"] = remove_zeros(ev["OBJECTID_3"])
         key = frozendict({"event_timestamp": ev["event_timestamp"],
                           "event_resource": ev["event_resource"], "event_activity": ev["event_activity"]})
         if key not in Shared.events:
             Shared.events[key] = set()
         Shared.events[key].add(frozendict({ev["OBJECTCLAS"]: ev["OBJECTID"]}))
+        Shared.events[key].add(frozendict({ev["OBJECTCLAS_3"]: ev["OBJECTID_3"]}))
+
+
+def extract_rbkp():
+    # keep only stuff in EKBE
+    ekbe = pd.read_csv(os.path.join(dir, "ekbe.tsv"), sep="\t")
+    belnr = list(ekbe["BELNR"])
+    rbkp = pd.read_csv(os.path.join(dir, "rbkp.tsv"), sep="\t")
+    rbkp = rbkp[["BELNR", "CPUDT", "CPUTM", "USNAM", "TCODE"]]
+    rbkp = rbkp[rbkp["BELNR"].isin(belnr)]
+    rbkp = rbkp.rename(columns={"USNAM": "event_resource", "TCODE": "event_activity"})
+    rbkp["event_timestamp"] = rbkp["CPUDT"] + " " + rbkp["CPUTM"]
+    rbkp["event_timestamp"] = pd.to_datetime(rbkp["event_timestamp"], format="%d.%m.%Y %H:%M:%S")
+    rbkp = rbkp.dropna(subset=["event_activity"])
+    rbkp = rbkp.dropna(subset=["event_resource"])
+    stream = rbkp.to_dict("r")
+    for ev in stream:
+        key = frozendict({"event_timestamp": ev["event_timestamp"],
+                          "event_resource": ev["event_resource"], "event_activity": ev["event_activity"]})
+        if key not in Shared.events:
+            Shared.events[key] = set()
+        Shared.events[key].add(frozendict({"BELNR": str(ev["BELNR"])}))
+
+
+def extract_bkpf():
+    # keep only stuff in EKBE
+    ekbe = pd.read_csv(os.path.join(dir, "ekbe.tsv"), sep="\t")
+    belnr = list(ekbe["BELNR"])
+    bkpf = pd.read_csv(os.path.join(dir, "bkpf.tsv"), sep="\t")
+    bkpf = bkpf[["BELNR", "CPUDT", "CPUTM", "USNAM", "TCODE"]]
+    bkpf = bkpf[bkpf["BELNR"].isin(belnr)]
+    bkpf = bkpf.rename(columns={"USNAM": "event_resource", "TCODE": "event_activity"})
+    bkpf["event_timestamp"] = bkpf["CPUDT"] + " " + bkpf["CPUTM"]
+    bkpf["event_timestamp"] = pd.to_datetime(bkpf["event_timestamp"], format="%d.%m.%Y %H:%M:%S")
+    bkpf = bkpf.dropna(subset=["event_activity"])
+    bkpf = bkpf.dropna(subset=["event_resource"])
+    stream = bkpf.to_dict("r")
+    for ev in stream:
+        key = frozendict({"event_timestamp": ev["event_timestamp"],
+                          "event_resource": ev["event_resource"], "event_activity": ev["event_activity"]})
+        if key not in Shared.events:
+            Shared.events[key] = set()
+        Shared.events[key].add(frozendict({"BELNR": str(ev["BELNR"])}))
+
+
+def extract_eban():
+    eban = pd.read_csv(os.path.join(dir, "eban.tsv"), sep="\t")
+    eban = eban[["BANFN", "ERDAT", "ERNAM"]]
+    eban = eban.rename(columns={"ERNAM": "event_resource", "ERDAT": "event_timestamp"})
+    eban["event_timestamp"] = pd.to_datetime(eban["event_timestamp"], format="%d.%m.%Y")
+    eban["event_activity"] = "ME51N"
+    eban = eban.dropna(subset=["event_activity"])
+    eban = eban.dropna(subset=["event_resource"])
+    stream = eban.to_dict("r")
+    for ev in stream:
+        key = frozendict({"event_timestamp": ev["event_timestamp"],
+                          "event_resource": ev["event_resource"], "event_activity": ev["event_activity"]})
+        if key not in Shared.events:
+            Shared.events[key] = set()
+        Shared.events[key].add(frozendict({"BANF": str(ev["BANFN"])}))
 
 
 def get_final_dataframe():
@@ -64,9 +137,14 @@ def get_final_dataframe():
 if __name__ == "__main__":
     read_activities()
     extract_cdhdr()
-    dataframe = get_final_dataframe()
-    print(dataframe)
-    model = discovery.apply(dataframe, model_type_variant="model2", node_freq_variant="type21",
-                            edge_freq_variant="type211")
-    gviz = vis_factory.apply(model, parameters={"format": "svg"})
-    vis_factory.view(gviz)
+    #extract_rbkp()
+    #extract_bkpf()
+    extract_eban()
+    if True:
+        dataframe = get_final_dataframe()
+        dataframe = clean_frequency.apply(dataframe, 20)
+    if True:
+        model = discovery.apply(dataframe, model_type_variant="model2", node_freq_variant="type21",
+                                edge_freq_variant="type211")
+        gviz = vis_factory.apply(model, parameters={"format": "svg", "min_edge_freq": 10})
+        vis_factory.view(gviz)
