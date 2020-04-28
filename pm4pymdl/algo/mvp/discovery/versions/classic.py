5a370051a793cd0ea3f792900410531fc0fede33
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-04-28 10:10:02 +0200

    small change

--- pm4pymdl/algo/mvp/discovery/versions/classic.py
+++ pm4pymdl/algo/mvp/discovery/versions/classic.py
@@ -6,17 +6,24 @@ from pm4py.algo.discovery.dfg.adapters.pandas import df_statistics
 from pm4py.algo.filtering.pandas.attributes import attributes_filter
 from pm4py.algo.filtering.pandas.end_activities import end_activities_filter
 from pm4py.algo.filtering.pandas.start_activities import start_activities_filter
-from pm4py.objects.heuristics_net import defaults
 from pm4py.objects.heuristics_net.net import HeuristicsNet
 from pm4py.util import constants
 import pandas as pd
-
 
 DEPENDENCY_THRESH = "dependency_thresh"
 AND_MEASURE_THRESH = "and_measure_thresh"
 MIN_ACT_COUNT = "min_act_count"
 MIN_DFG_OCCURRENCES = "min_dfg_occurrences"
 DFG_PRE_CLEANING_NOISE_THRESH = "dfg_pre_cleaning_noise_thresh"
+LOOP_LENGTH_TWO_THRESH = "loop_length_two_thresh"
+
+DEFAULT_DEPENDENCY_THRESH = -0.9
+DEFAULT_AND_MEASURE_THRESH = 0.65
+DEFAULT_MIN_ACT_COUNT = 1
+DEFAULT_MIN_DFG_OCCURRENCES = 1
+DEFAULT_DFG_PRE_CLEANING_NOISE_THRESH = -0.05
+DEFAULT_LOOP_LENGTH_TWO_THRESH = 0.5
+
 DECREASING_FACTOR = "decreasingFactor"
 PERFORMANCE = "performance"
 PERSPECTIVES = "perspectives"
@@ -85,14 +92,14 @@ def apply(df, parameters=None):
         df = pd.DataFrame({"event_id": [], "event_activity": []})
 
     dependency_thresh = parameters[
-        DEPENDENCY_THRESH] if DEPENDENCY_THRESH in parameters else defaults.DEFAULT_DEPENDENCY_THRESH
+        DEPENDENCY_THRESH] if DEPENDENCY_THRESH in parameters else DEFAULT_DEPENDENCY_THRESH
     and_measure_thresh = parameters[
-        AND_MEASURE_THRESH] if AND_MEASURE_THRESH in parameters else defaults.DEFAULT_AND_MEASURE_THRESH
-    min_act_count = parameters[MIN_ACT_COUNT] if MIN_ACT_COUNT in parameters else defaults.DEFAULT_MIN_ACT_COUNT
+        AND_MEASURE_THRESH] if AND_MEASURE_THRESH in parameters else DEFAULT_AND_MEASURE_THRESH
+    min_act_count = parameters[MIN_ACT_COUNT] if MIN_ACT_COUNT in parameters else DEFAULT_MIN_ACT_COUNT
     min_dfg_occurrences = parameters[
-        MIN_DFG_OCCURRENCES] if MIN_DFG_OCCURRENCES in parameters else defaults.DEFAULT_MIN_DFG_OCCURRENCES
+        MIN_DFG_OCCURRENCES] if MIN_DFG_OCCURRENCES in parameters else DEFAULT_MIN_DFG_OCCURRENCES
     dfg_pre_cleaning_noise_thresh = parameters[
-        DFG_PRE_CLEANING_NOISE_THRESH] if DFG_PRE_CLEANING_NOISE_THRESH in parameters else defaults.DEFAULT_DFG_PRE_CLEANING_NOISE_THRESH
+        DFG_PRE_CLEANING_NOISE_THRESH] if DFG_PRE_CLEANING_NOISE_THRESH in parameters else DEFAULT_DFG_PRE_CLEANING_NOISE_THRESH
     decreasing_factor_sa_ea = parameters[DECREASING_FACTOR] if DECREASING_FACTOR in parameters else 0.5
     performance = parameters[PERFORMANCE] if PERFORMANCE in parameters else False
     perspectives = parameters[PERSPECTIVES] if PERSPECTIVES in parameters else None
