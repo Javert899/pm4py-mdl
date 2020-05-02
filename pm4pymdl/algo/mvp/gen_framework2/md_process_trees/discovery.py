60b888e06228a519878d611d8f495dc9f77eb645
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-05-02 10:01:07 +0200

    personalized colors
    personalized colors

--- pm4pymdl/algo/mvp/gen_framework2/md_process_trees/discovery.py
+++ pm4pymdl/algo/mvp/gen_framework2/md_process_trees/discovery.py
@@ -4,6 +4,8 @@ from pm4pymdl.algo.mvp.utils import clean_frequency, clean_arc_frequency
 from pm4py.objects.conversion.log import factory as log_conv_factory
 from pm4py.objects.log.log import EventStream
 from pm4py.algo.discovery.inductive import factory as inductive_miner
+from collections import Counter
+
 
 def apply(df0, classifier_function=None, parameters=None):
     if parameters is None:
@@ -25,14 +27,22 @@ def apply(df0, classifier_function=None, parameters=None):
     activities = df.groupby("event_id").first().reset_index()["event_activity"].value_counts()
 
     obj_types = [x for x in df.columns if not x.startswith("event_")]
+    activities_repeated = Counter()
+
     for ot in obj_types:
         new_df = df[["event_id", "event_activity", "event_timestamp", ot]].dropna(subset=[ot])
         new_df = new_df.rename(
             columns={ot: "case:concept:name", "event_activity": "concept:name", "event_timestamp": "time:timestamp"})
         log = new_df.to_dict("r")
         log = EventStream(log)
+        this_activities = set(x["concept:name"] for x in log)
+        for act in this_activities:
+            activities_repeated[act] += 1
         log = log_conv_factory.apply(log, variant=log_conv_factory.TO_EVENT_LOG)
 
         models[ot] = inductive_miner.apply_tree(log, parameters=parameters)
 
-    return {"type": "ptree", "models": models, "activities": activities}
+    activities_repeated = set(x for x in activities_repeated if activities_repeated[x] > 1)
+
+    return {"type": "ptree", "models": models, "activities": activities, "activities_repeated": activities_repeated}
+
--- pm4pymdl/visualization/mvp/gen_framework2/versions/ptree.py
+++ pm4pymdl/visualization/mvp/gen_framework2/versions/ptree.py
@@ -3,13 +3,12 @@ import uuid
 from graphviz import Digraph
 from pm4py.objects.process_tree import pt_operator
 
-
 COLORS = ["#05B202", "#A13CCD", "#39F6C0", "#BA0D39", "#E90638", "#07B423", "#306A8A", "#678225", "#2742FE", "#4C9A75",
           "#4C36E9", "#7DB022", "#EDAC54", "#EAC439", "#EAC439", "#1A9C45", "#8A51C4", "#496A63", "#FB9543", "#2B49DD",
           "#13ADA5", "#2DD8C1", "#2E53D7", "#EF9B77", "#06924F", "#AC2C4D", "#82193F", "#0140D3"]
 
 
-def repr_tree(tree, viz, current_node, rec_depth, acti_map, persp_color, parameters):
+def repr_tree(tree, viz, current_node, rec_depth, res, acti_map, persp_color, parameters):
     """
     Represent a subtree on the GraphViz object
 
@@ -37,9 +36,17 @@ def repr_tree(tree, viz, current_node, rec_depth, acti_map, persp_color, paramet
                      fontsize="8")
             this_trans_id = str(uuid.uuid4())
             if child.label is None:
-                viz.node(this_trans_id, "tau", style='filled', fillcolor=persp_color)
+                viz.node(this_trans_id, "tau", style='filled', color=persp_color, fontcolor=persp_color,
+                         fillcolor="white")
             else:
-                viz.node(this_trans_id, str(child))
+                if not str(child) in acti_map:
+                    acti_map[str(child)] = this_trans_id
+                    if str(child) in res["activities_repeated"]:
+                        viz.node(this_trans_id, str(child), style="filled", fillcolor="white")
+                    else:
+                        viz.node(this_trans_id, str(child), style="filled", fillcolor=persp_color)
+                else:
+                    this_trans_id = acti_map[str(child)]
             viz.edge(current_node, this_trans_id, color=persp_color)
         else:
             condition_wo_operator = child.operator == pt_operator.Operator.XOR and len(
@@ -50,9 +57,17 @@ def repr_tree(tree, viz, current_node, rec_depth, acti_map, persp_color, paramet
                          fontsize="8")
                 this_trans_id = str(uuid.uuid4())
                 if childchild.label is None:
-                    viz.node(this_trans_id, str(childchild), style='filled', fillcolor=persp_color)
+                    viz.node(this_trans_id, str(childchild), style='filled', color=persp_color, fontcolor=persp_color,
+                             fillcolor="white")
                 else:
-                    viz.node(this_trans_id, str(childchild))
+                    if not str(childchild) in acti_map:
+                        acti_map[str(childchild)] = this_trans_id
+                        if str(childchild) in res["activities_repeated"]:
+                            viz.node(this_trans_id, str(childchild), style="filled", fillcolor="white")
+                        else:
+                            viz.node(this_trans_id, str(childchild), style="filled", fillcolor=persp_color)
+                    else:
+                        this_trans_id = acti_map[str(childchild)]
                 viz.edge(current_node, this_trans_id, color=persp_color)
             else:
                 viz.attr('node', shape='circle', fixedsize='true', width="0.6",
@@ -60,8 +75,9 @@ def repr_tree(tree, viz, current_node, rec_depth, acti_map, persp_color, paramet
                 op_node_identifier = str(uuid.uuid4())
                 viz.node(op_node_identifier, str(child.operator))
                 viz.edge(current_node, op_node_identifier, color=persp_color)
-                viz = repr_tree(child, viz, op_node_identifier, rec_depth + 1, acti_map, persp_color, parameters)
-    return viz
+                viz, acti_map = repr_tree(child, viz, op_node_identifier, rec_depth + 1, res, acti_map, persp_color,
+                                          parameters)
+    return viz, acti_map
 
 
 def apply(res, parameters=None):
@@ -85,19 +101,28 @@ def apply(res, parameters=None):
             op_node_identifier = str(uuid.uuid4())
             viz.node(op_node_identifier, str(tree.operator))
 
-            viz = repr_tree(tree, viz, op_node_identifier, 0, acti_map, persp_color, parameters)
+            viz, acti_map = repr_tree(tree, viz, op_node_identifier, 0, res, acti_map, persp_color, parameters)
         else:
             viz.attr('node', shape='box', fixedsize='true', width="2.5",
                      fontsize="8")
             this_trans_id = str(uuid.uuid4())
             if tree.label is None:
-                viz.node(this_trans_id, "tau", style='filled', fillcolor=persp_color)
+                viz.node(this_trans_id, "tau", style='filled', color=persp_color, fontcolor=persp_color,
+                         fillcolor="white")
             else:
-                viz.node(this_trans_id, str(tree))
+                if not str(tree) in acti_map:
+                    acti_map[str(tree)] = this_trans_id
+                    if str(tree) in res["activities_repeated"]:
+                        viz.node(this_trans_id, str(tree), style="filled", fillcolor="white")
+                    else:
+                        viz.node(this_trans_id, str(tree), style="filled", fillcolor=persp_color)
+                else:
+                    this_trans_id = acti_map[str(tree)]
+
+        print(count, acti_map)
 
     viz.attr(overlap='false')
     viz.attr(fontsize='11')
     viz.format = image_format
 
     return viz
-
