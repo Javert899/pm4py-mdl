4d1bf7b64dc552b7b0803a5f3a92c2b1f7a2640e
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-06-04 15:54:26 +0200

    p2p

--- p2p_mdl.py
+++ p2p_mdl.py
@@ -8,6 +8,8 @@ class Shared:
     MSEG_mblnr_matnr = {}
     RSEG_belnr_matnr = {}
     EKKO_events = {}
+    MKPF_events = {}
+    RBKP_events = {}
 
 
 def read_ekbe():
@@ -57,8 +59,41 @@ def read_ekko():
     for el in stream:
         if not el["EBELN"] in Shared.EKKO_events:
             Shared.EKKO_events[el["EBELN"]] = list()
-        Shared.EKKO_events[el["EBELN"]].append(frozendict({"event_activity": "ME21N", "event_timestamp": el["AEDAT"]}))
+        Shared.EKKO_events[el["EBELN"]].append({"event_activity": "ME21N", "event_timestamp": el["AEDAT"]})
         Shared.EKKO_events[el["EBELN"]] = sorted(Shared.EKKO_events[el["EBELN"]], key=lambda x: x["event_timestamp"])
+
+
+def read_mkpf():
+    df = pd.read_csv("MKPF.tsv", sep="\t",
+                     dtype={"MBLNR": str, "CPUDT": str, "CPUTM": str, "USNAM": str, "TCODE": str, "TCODE2": str})
+    df["event_timestamp"] = df["CPUDT"] + " " + df["CPUTM"]
+    df["event_timestamp"] = pd.to_datetime(df["event_timestamp"], format="%d.%m.%Y %H:%M:%S", errors='coerce')
+    stream = df.to_dict('r')
+    for el in stream:
+        if str(el["TCODE"]).lower() != "nan":
+            if not el["MBLNR"] in Shared.MKPF_events:
+                Shared.MKPF_events[el["MBLNR"]] = list()
+            Shared.MKPF_events[el["MBLNR"]].append(
+                {"event_activity": el["TCODE"], "event_timestamp": el["event_timestamp"],
+                 "event_resource": el["USNAM"]})
+            Shared.MKPF_events[el["MBLNR"]] = sorted(Shared.MKPF_events[el["MBLNR"]],
+                                                     key=lambda x: x["event_timestamp"])
+
+
+def read_rbkp():
+    df = pd.read_csv("RBKP.tsv", sep="\t", dtype={"BELNR": str, "USNAM": str, "TCODE": str, "CPUDT": str, "CPUTM": str})
+    df["event_timestamp"] = df["CPUDT"] + " " + df["CPUTM"]
+    df["event_timestamp"] = pd.to_datetime(df["event_timestamp"], format="%d.%m.%Y %H:%M:%S", errors='coerce')
+    stream = df.to_dict('r')
+    for el in stream:
+        if str(el["TCODE"]).lower() != "nan":
+            if not el["BELNR"] in Shared.RBKP_events:
+                Shared.RBKP_events[el["BELNR"]] = list()
+            Shared.RBKP_events[el["BELNR"]].append(
+                {"event_activity": el["TCODE"], "event_timestamp": el["event_timestamp"],
+                 "event_resource": el["USNAM"]})
+            Shared.RBKP_events[el["BELNR"]] = sorted(Shared.RBKP_events[el["BELNR"]],
+                                                     key=lambda x: x["event_timestamp"])
 
 
 if __name__ == "__main__":
@@ -67,8 +102,10 @@ if __name__ == "__main__":
     read_mseg()
     read_rseg()
     read_ekko()
-    #print(Shared.EKBE_ebeln_belnr)
-    #print(Shared.EKPO_ebeln_matnr)
-    #print(Shared.MSEG_mblnr_matnr)
-    #print(Shared.RSEG_belnr_matnr)
-    print(Shared.EKKO_events)
+    read_mkpf()
+    read_rbkp()
+    # print(Shared.EKBE_ebeln_belnr)
+    # print(Shared.EKPO_ebeln_matnr)
+    # print(Shared.MSEG_mblnr_matnr)
+    # print(Shared.RSEG_belnr_matnr)
+    print(Shared.RBKP_events)
