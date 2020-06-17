ceb24c76cc2a8ec13f3ad894719a23a92272e484
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-06-17 08:07:35 +0200

    converting timestamp columns

--- pm4pymdl/objects/sqlite/importer/factory.py
+++ pm4pymdl/objects/sqlite/importer/factory.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 import pandas as pd
+from pm4py.objects.log.util import dataframe_utils
 
 
 def apply(file_path, return_obj_dataframe=False, parameters=None):
@@ -10,18 +11,24 @@ def apply(file_path, return_obj_dataframe=False, parameters=None):
     db = sqlite3.connect(file_path)
 
     df = pd.read_sql_query("SELECT * FROM __EVENTS_TABLE", db)
+    df = dataframe_utils.convert_timestamp_columns_in_df(df)
 
     if return_obj_dataframe:
         ot_df = pd.read_sql_query("SELECT * FROM __OBJECT_TYPES", db)
+
         OT = list(ot_df["NAME"])
 
         obj_df_list = []
 
         for ot in OT:
             o_df = pd.read_sql_query("SELECT * FROM " + ot, db)
+            o_df = dataframe_utils.convert_timestamp_columns_in_df(o_df)
 
             obj_df_list.append(o_df)
 
         obj_df = pd.concat(obj_df_list)
 
-    return df, obj_df
+        return df, obj_df
+
+    return df
+
