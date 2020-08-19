35e78fa987e6a30b125a1033cbf3050418821f9f
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-08-19 16:56:50 +0200

    intermediate commit

--- pm4pymdl/algo/mvp/utils/build_graph.py
+++ pm4pymdl/algo/mvp/utils/build_graph.py
@@ -2,6 +2,7 @@ import networkx
 from pm4py.objects.log.log import EventLog, Trace, Event
 from datetime import datetime
 from pm4py.objects.log.util import sorting
+from collections import Counter
 
 
 def apply(df, source_attr, target_attr, type_attr, timestamp_key, reverse=False):
@@ -37,6 +38,33 @@ def apply(df, source_attr, target_attr, type_attr, timestamp_key, reverse=False)
     return G, conn_comp, timestamps
 
 
+def describe_graph(G, comp):
+    SG = G.subgraph(comp)
+    nodes = list(SG.nodes)
+    edges = list(SG.edges)
+    nodes = [(x, y) for x, y in Counter(n.split("=")[0] for n in nodes).items()]
+    for i in range(len(nodes)):
+        if nodes[i][1] > 1:
+            nodes[i] = (nodes[i][0], "N")
+        else:
+            nodes[i] = (nodes[i][0], "1")
+    source0 = {n[0]: [e for e in edges if e[0].split("=")[0] == n[0]] for n in nodes}
+    target0 = {n[0]: [e for e in edges if e[1].split("=")[0] == n[0]] for n in nodes}
+    source1 = {x: {} for x in source0}
+    target1 = {x: {} for x in target0}
+    for x in source0:
+        ik = set(y[0] for y in source0[x])
+        for k in ik:
+            source1[x][k.split("=")[1]] = Counter(y[1].split("=")[0] for y in source0[x] if y[0] == k)
+    for x in target0:
+        ik = set(y[1] for y in target0[x])
+        for k in ik:
+            target1[x][k.split("=")[1]] = Counter(y[0].split("=")[0] for y in source0[x] if y[1] == k)
+    print(source1)
+    print(target1)
+    # print(edges)
+
+
 def create_log(G, conn_comp, timestamps, max_comp_len=50, include_loops=False):
     log = EventLog()
     for i in range(len(conn_comp)):
@@ -51,7 +79,7 @@ def create_log(G, conn_comp, timestamps, max_comp_len=50, include_loops=False):
                     SGG.remove_edge(e[0], e[1])
             sorted_nodes = list(networkx.topological_sort(SGG))
             for n in sorted_nodes:
-                selfloop = 1 if (n,n) in SG.edges else 0
+                selfloop = 1 if (n, n) in SG.edges else 0
                 trace.append(Event({'time:timestamp': timestamps[n.split("=")[1]], 'concept:name': n.split("=")[0],
                                     'value': n.split("=")[1], 'typevalue': n, 'selfloop': selfloop}))
                 if include_loops and selfloop:
