1562d0b4dc9cac01c4b74fe6919548c8572c72d3
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2020-10-02 08:08:44 +0200

    fix(pm4pymdl): fixed import
    fixed import

--- pm4pymdl/objects/jmd/importer/factory.py
+++ pm4pymdl/objects/jmd/importer/factory.py
@@ -3,7 +3,7 @@ import pandas as pd
 from dateutil import parser
 
 
-def apply(file_path, return_obj_df=True, parameters=None):
+def apply(file_path, return_obj_df=None, parameters=None):
     if parameters is None:
         parameters = {}
 
@@ -14,8 +14,28 @@ def apply(file_path, return_obj_df=True, parameters=None):
     eve_stream = obj[prefix+"events"]
     for el in eve_stream:
         eve_stream[el]["event_id"] = el
+    obj_stream = obj[prefix+"objects"]
+    for el in obj_stream:
+        obj_stream[el]["object_id"] = el
+    obj_stream = list(obj_stream.values())
+    obj_type = {}
+    for el in obj_stream:
+        obj_type[el["object_id"]] = el[prefix+"otyp"]
+        del el[prefix+"otyp"]
+        for k2 in el[prefix+"ovmap"]:
+            el["object_"+k2] = el[prefix+"ovmap"][k2]
+        del el[prefix+"ovmap"]
     eve_stream = list(eve_stream.values())
     for el in eve_stream:
+        new_omap = {}
+        for obj in el[prefix+"omap"]:
+            typ = obj_type[obj]
+            if not typ in new_omap:
+                new_omap[typ] = set()
+            new_omap[typ].add(obj)
+        for typ in new_omap:
+            new_omap[typ] = list(new_omap[typ])
+        el[prefix+"omap"] = new_omap
         el["event_activity"] = el[prefix+"activity"]
         el["event_timestamp"] = parser.parse(el[prefix+"timestamp"])
         del el[prefix+"activity"]
@@ -26,23 +46,13 @@ def apply(file_path, return_obj_df=True, parameters=None):
         for k2 in el[prefix+"omap"]:
             el[k2] = el[prefix+"omap"][k2]
         del el[prefix + "omap"]
-    obj_stream = obj[prefix+"objects"]
-    for el in obj_stream:
-        obj_stream[el]["object_id"] = el
-    obj_stream = list(obj_stream.values())
-    for el in obj_stream:
-        el["object_type"] = el[prefix+"otyp"]
-        del el[prefix+"otyp"]
-        for k2 in el[prefix+"ovmap"]:
-            el["object_"+k2] = el[prefix+"ovmap"][k2]
-        del el[prefix+"ovmap"]
 
     eve_df = pd.DataFrame(eve_stream)
     obj_df = pd.DataFrame(obj_stream)
 
     eve_df.type = "succint"
 
-    if return_obj_df:
+    if return_obj_df or (return_obj_df is None and len(obj_df.columns) > 1):
         return eve_df, obj_df
     return eve_df
 
