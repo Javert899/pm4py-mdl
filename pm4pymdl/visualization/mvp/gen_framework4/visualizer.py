aba7817eff507f528dcb453caa00bb966481427e
Alessandro Berti <javert@northwood.northwood.net>
2020-10-09 14:32:45 +0200

    bug fix
    bug fix

--- pm4pymdl/visualization/mvp/gen_framework4/visualizer.py
+++ pm4pymdl/visualization/mvp/gen_framework4/visualizer.py
@@ -42,7 +42,7 @@ def human_readable_stat(c):
     return str(seconds) + "s"
 
 
-def apply(model, measure="performance", freq="events", classifier="activity", projection="no", count_events=False, parameters=None):
+def apply(model, measure="frequency", freq="events", classifier="activity", projection="no", count_events=False, parameters=None):
     if parameters is None:
         parameters = {}
 
